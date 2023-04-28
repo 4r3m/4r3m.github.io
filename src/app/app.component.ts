92c82081d7dd2b920a415dd074d6206e0c710aa2
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-04-28 19:58:06 +0800

    Added all needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setButtonListeners();
+  }
+
+  private setButtonListeners() {
+    document.querySelectorAll(".control").forEach((button: Element) => {
+      button.addEventListener("click", function(this: HTMLElement) {
+        const id = this.getAttribute("data-id");
+        if (id) {
+          document.querySelector(".active-btn")!.classList.remove("active-btn");
+          this.classList.add("active-btn");
+          document.querySelector(".active")!.classList.remove("active");
+          document.getElementById(id)!.classList.add("active");
+        }
+      });
+    });
+
+    document.querySelector(".theme-btn")!.addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
+    });
+  }
 }
