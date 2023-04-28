9d9f5f75a07f2d2b34606317a14c19e8e56aaf46
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-04-29 01:02:51 +0800

    optimized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,24 @@ export class AppComponent implements OnInit {
     this.setButtonListeners();
   }
 
+  downloadMyFile() {
+    // Create a link element
+    const link = document.createElement('a');
+    // Set the target attribute to '_blank' to open the file in a new tab
+    link.setAttribute('target', '_blank');
+    // Set the href attribute to the URL of the file you want to download
+    link.setAttribute('href', '/assets/files/RaymartSalvador.pdf');
+    // Set the download attribute to the desired filename for the downloaded file
+    link.setAttribute('download', 'RaymartSalvador.pdf');
+    // Append the link element to the body of the page
+    document.body.appendChild(link);
+    // Simulate a click on the link to initiate the download
+    link.click();
+    // Remove the link element from the page once the download has started
+    link.remove();
+    // Log a message to the console to indicate that the download has started
+    console.log('Download started');
+  }
   private setButtonListeners() {
     document.querySelectorAll(".control").forEach((button: Element) => {
       button.addEventListener("click", function(this: HTMLElement) {
