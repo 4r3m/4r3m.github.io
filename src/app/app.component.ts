e8fa5aac379fb3d95f79b43624038482cd07dcde
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-02 18:31:43 +0800

    perf(root): :construction: slicing component scss file too big

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', ]
 })
 export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  function:Function = this.downloadMyFile;
+  buttonText = 'Downlod CV'
+  fontAwesome = 'fas fa-download'
+
+
 
   constructor() {}
 
