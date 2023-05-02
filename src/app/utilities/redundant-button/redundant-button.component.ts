377f0eb7020ba371d0f0f111e20ed8cbf0846010
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-03 00:40:31 +0800

    google api

--- src/app/utilities/redundant-button/redundant-button.component.ts
+++ src/app/utilities/redundant-button/redundant-button.component.ts
@@ -11,23 +11,24 @@ export class RedundantButtonComponent {
   @Input() fontAwesome = '';
 
   whatFunction() {
-        // Create a link element
-        const link = document.createElement('a');
-        // Set the target attribute to '_blank' to open the file in a new tab
-        link.setAttribute('target', '_blank');
-        // Set the href attribute to the URL of the file you want to download
-        link.setAttribute('href', '/assets/files/RaymartSalvador.pdf');
-        // Set the download attribute to the desired filename for the downloaded file
-        link.setAttribute('download', 'RaymartSalvador.pdf');
-        // Append the link element to the body of the page
-        document.body.appendChild(link);
-        // Simulate a click on the link to initiate the download
-        link.click();
-        // Remove the link element from the page once the download has started
-        link.remove();
-        // Log a message to the console to indicate that the download has started
-        console.log('Download started');
-
-    }
+    // Create a link element
+    const link = document.createElement('a');
+    // Set the target attribute to '_blank' to open the file in a new tab
+    link.setAttribute('target', '_blank');
+    // Set the href attribute to the URL of the file you want to download
+    link.setAttribute(
+      'href',
+      'https://drive.google.com/file/d/1totVw8BfLm3RyI5nT7Tf8IISbmDo-4dG/view?usp=share_link'
+    );
+    // Set the download attribute to the desired filename for the downloaded file
+    link.setAttribute('download', 'RaymartSalvador.pdf');
+    // Append the link element to the body of the page
+    document.body.appendChild(link);
+    // Simulate a click on the link to initiate the download
+    link.click();
+    // Remove the link element from the page once the download has started
+    link.remove();
+    // Log a message to the console to indicate that the download has started
+    console.log('Download started');
   }
-
+}
