aaa0d8aaee93f32951ed9295795f43a519df17ee
Raymart Salvador <rgsalvador@iskolarngbayan.pup.edu.ph>
2023-05-04 20:22:35 +0800

    perf(environment): :fire: reduce unnecessary code

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production:true,
-  hostURL: 'https://4r3m.github.io/',
-  assetsDir: 'https://github.com/4r3m/4r3m.github.io/tree/master/docs',
-  username: 'userDev1',
-  token: 'devToken',
-  envName: 'dev'
+  assetsDir: 'https://github.com/4r3m/4r3m.github.io/tree/master/docs'
 };
