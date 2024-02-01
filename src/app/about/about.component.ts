ab9d2084f261def7e6ae8bcc257d96789cc537f0
Raymart Salvador <salvadorraymartg@gmail.com>
2024-02-02 04:18:12 +0800

    feat(component): :sparkles: add certs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,9 +11,58 @@ export class AboutComponent {
   constructor(private title: Title) {
     title.setTitle('Raymart Salvador');
   }
+
   fontAwesome = 'fas fa-download';
   buttonText = 'Download CV';
   _ASSETS = environment._ASSETS;
+  showModal = false;
+  modalAsset = '';
+  certificates = [
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/1.png',
+      title: 'Cyber Security: Guide and Awareness',
+      description:
+        'A seminar about how vulnerable we are in the world of internet. ',
+    },
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/2.png',
+      title: 'Business Analysis: Planning in Creating a Requirements Document',
+      description: 'A discussion that involves the process before development',
+    },
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/3.png',
+      title: 'UI/UX Design: Design Fundamentals for Beginners',
+      description:
+        'It gave a glimpse of the trending tools used in UI/UX design.',
+    },
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/4.png',
+      title:
+        'Project Management Fundamentals and Customer Support as the Frontline of an Organization',
+      description: 'A talk of a perspective of a project manager',
+    },
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/5.png',
+      title: 'Database and Systems Administration for Beginners',
+      description:
+        'A live demonstration of SQL database things and how to manage it.',
+    },
+    {
+      url: this._ASSETS + '/assets/images/my-certificates/6.png',
+      title: 'Introduction to Data Analysis with Excel and Power BI tools',
+      description:
+        'A live demonstration of how to actually use the data from filtering to displaying the output.',
+    },
+  ];
+
+  enlargeImage(asset: string) {
+    this.modalAsset = asset;
+    this.showModal = true;
+  }
+
+  closeImageModal() {
+    this.showModal = false;
+  }
 
   downloadMyFile() {
     // Create a link element
