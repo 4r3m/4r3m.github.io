cf8ce5c2e085878ac69d99f9ceb97569c3cdd11c
Raymart Salvador <salvadorraymartg@gmail.com>
2024-03-15 11:17:35 +0800

    update specialization

--- src/app/utilities/const/main.ts
+++ src/app/utilities/const/main.ts
@@ -1,4 +1,4 @@
-export const SPECIALIZATION = 'Frontend Developer';
+export const SPECIALIZATION = 'Web Developer';
 export const FULL_NAME = 'Raymart Salvador';
 export const SELF_DESCRIPTION =
   'Someone who excels at combining dedicated hard work with smart strategies, ensuring optimal productivity and impressive outcomes';
